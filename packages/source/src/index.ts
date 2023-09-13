070d253d85f23347914646076e130f89eebaab31
Azrideus <22293394+Azrideus@users.noreply.github.com>
2023-09-13 20:10:00 +0300

    export chatmanager

--- packages/source/src/index.ts
+++ packages/source/src/index.ts
@@ -1,3 +1,5 @@
 export * from './views/VirtualScroller';
+export * from './classes/ChatManager';
+export * from './classes/ChatItem'; 
 import VirtualScroller from './views/VirtualScroller';
 export default VirtualScroller;
